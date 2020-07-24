94f0c5619c08a3b36c252e57240698ba91a2f482
shivangikakkar <shivangikakkar15@gmail.com>
2020-07-24 18:10:55 +0530

    Banner

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './App.css';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Header from './Header';
+import Home from './Home';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <div className="app">
         <Switch>
           <Route path="/checkout">
+            <Header />
             <h1>Checkout</h1>
           </Route>
           <Route path="/login">
@@ -17,7 +19,7 @@ function App() {
           {/* THIS IS THE DEFAULT ROUTE */}
           <Route path="/"> 
             <Header />
-            <h1>HOME PAGE!!!</h1>
+            <Home />
           </Route>
         </Switch>
       </div>
